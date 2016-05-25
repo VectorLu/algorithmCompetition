8570d1f6e79ecb5aae11d3fedd6c0179cb993d60
VectorLu <vectorpinglu@outlook.com>
2016-05-25 21:44:28 +0800

    delete test line

--- ch2/pi.c
+++ ch2/pi.c
@@ -9,19 +9,14 @@ int main()
   for(int i = 0; ; i++)
   {
     sum = sum + delta;
-    printf("sum = %f\n", sum);
-    printf("n = %f\n", n);
     n += 2;
-    printf("n = %f\n", n);
     if(i%2 == 0)
     {
       delta = -1/n;
-      printf("delta = %f\n", delta);
     }
     else
     {
       delta = 1/n;
-      printf("delta = %f\n", delta);
     }
     if(fabs(delta) <= 1e-6)
     {
