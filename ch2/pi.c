3031142688553a7f53bc469269deccec82bc2609
VectorLu <vectorpinglu@outlook.com>
2016-05-25 21:43:23 +0800

    initialize n
    double n, delta = 1.0 just initialize delta

--- ch2/pi.c
+++ ch2/pi.c
@@ -4,11 +4,13 @@
 int main()
 {
   double sum = 0.0;
-  double n, delta = 1.0;
+  double delta = 1.0;
+  double n = 1.0;
   for(int i = 0; ; i++)
   {
     sum = sum + delta;
     printf("sum = %f\n", sum);
+    printf("n = %f\n", n);
     n += 2;
     printf("n = %f\n", n);
     if(i%2 == 0)
@@ -21,7 +23,7 @@ int main()
       delta = 1/n;
       printf("delta = %f\n", delta);
     }
-    if(fabs(delta) < 1e-6)
+    if(fabs(delta) <= 1e-6)
     {
       break;
     }
