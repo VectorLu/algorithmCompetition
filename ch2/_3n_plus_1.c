8387c2f2a64cac6a530ca6514c0858e8e1ad6f30
VectorLu <vectorpinglu@outlook.com>
2016-05-25 20:29:48 +0800

    3n + 1 problem

---
+++ ch2/_3n_plus_1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+  int n2, count = 0;
+  scanf("%d", &n);
+  long long n = n2;
+  while(n > 1)
+  {
+    if(n % 2 == 1)
+    {
+      n = 3*n + 1;
+    }
+    else
+    {
+      n /= 2;
+    }
+    count++;
+  }
+  printf("%d\n", count);
+}
