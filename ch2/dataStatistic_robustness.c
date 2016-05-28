b800ecf5dbdf5c61742ffdda2f456aa1701f8439
VectorLu <vectorpinglu@outlook.com>
2016-05-28 09:14:11 +0800

    finish dataStatistic_robustness.c

--- ch2/dataStatistic_robustness.c
+++ ch2/dataStatistic_robustness.c
@@ -2,5 +2,27 @@
 #define INF 1000000
 int main()
 {
-  
+    int x, n = 0, s = 0, kase = 0, min = INF, max = -INF;
+    while(scanf("%d", &n) && n)
+    {
+      for(int i = 0; i < n; i++)
+      {
+        scanf("%d", &x);
+        if(min > x)
+        {
+          min = x;
+        }
+        if(max < x)
+        {
+          max = x;
+        }
+        s += x;
+      }
+      if(kase)
+      {
+        printf("\n");
+      }
+      printf("case%d: min = %d, max = %d, average = %f\n", ++kase, min, max, (double)s/n);
+    }
+    return 0;
 }
