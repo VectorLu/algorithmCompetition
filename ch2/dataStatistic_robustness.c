90eb7d5133e949ef68f72852270c6f9f9ecfe226
VectorLu <vectorpinglu@outlook.com>
2016-05-28 09:22:09 +0800

    在循环中重置变量，保留几位小数要注意题目要求，%f默认保留6位小数

--- ch2/dataStatistic_robustness.c
+++ ch2/dataStatistic_robustness.c
@@ -2,9 +2,10 @@
 #define INF 1000000
 int main()
 {
-    int x, n = 0, s = 0, kase = 0, min = INF, max = -INF;
+    int x, n = 0, kase = 0;
     while(scanf("%d", &n) && n)
     {
+      int s = 0, min = INF, max = -INF;
       for(int i = 0; i < n; i++)
       {
         scanf("%d", &x);
@@ -22,7 +23,7 @@ int main()
       {
         printf("\n");
       }
-      printf("case%d: min = %d, max = %d, average = %f\n", ++kase, min, max, (double)s/n);
+      printf("case%d: min = %d, max = %d, average = %.3f\n", ++kase, min, max, (double)s/n);
     }
     return 0;
 }
