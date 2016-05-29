6b5590ee63146ebd702e4bafeee78d62b8113069
VectorLu <vectorpinglu@outlook.com>
2016-05-29 23:27:15 +0800

    Update turnOnLight.c

--- ch3/turnOnLight.c
+++ ch3/turnOnLight.c
@@ -1,9 +1,11 @@
+/*有n盏灯，编号为1-n。第1个人把所有灯打开，第2个人按下*/
 #include <stdio.h>
 #include <string.h>
 #define maxn 1010
-int a[maxn];
+int light[maxn];
 
 int main()
 {
-  int n, k
+  int n, k, first = 1;
+  memset(a, 0, sizeof(a))
 }
