bc089180c53a89f0340280feede8f758ad847cf9
VectorLu <vectorpinglu@outlook.com>
2016-05-29 16:14:54 +0800

    虽然不是非常严谨，但是能够求出正确答案

--- ch2/permutation.c
+++ ch2/permutation.c
@@ -1,8 +1,33 @@
-void abcdefghi()
+#include <stdio.h>
+void result(int num, int *result_add, int *result_multi)
 {
+  int a = num/100;
+  int b = num/10%10;
+  int c = num%10;
+
+  *result_add += (a+b+c);
+  *result_multi *= (a*b*c);
+}
+
+int main()
+{
+  int result_add , result_multi;
   for(int abc = 100; abc < 333; abc++)
   {
     int def = 2*abc;
     int ghi = 3*abc;
+
+    result_add = 0;
+    result_multi = 1;
+
+    result(abc, &result_add, &result_multi);
+    result(def, &result_add, &result_multi);
+    result(ghi, &result_add, &result_multi);
+
+    if(result_add == 45 && result_multi == 362880)
+    {
+      printf("%d %d %d\n", abc, def, ghi);
+    }
   }
+  return 0;
 }
