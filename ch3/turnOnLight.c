937deeb4840ae03196d9f4c859c79073619d72f2
VectorLu <vectorpinglu@outlook.com>
2016-05-30 08:05:10 +0800

    Update turnOnLight.c

--- ch3/turnOnLight.c
+++ ch3/turnOnLight.c
@@ -1,4 +1,3 @@
-//test
 /*
  有n盏灯，编号为1-n。第1个人把所有灯打开，第2个人按下所有编号为2
  的倍数的开关（这些灯将被关掉），第3个人按下所有编号为3的倍数的开关
@@ -13,8 +12,11 @@ int light[maxn];
 int main()
 {
     int n, k, first = 1;
+    //将light数组清零
     memset(light, 0, sizeof(light));
     scanf("%d%d", &n, &k);
+    //模拟开关灯，当i = 1的时候，j%i == 0
+    //light[j]都变为1，即为开灯
     for(int i = 1; i <= k; i++)
     {
         for(int j = 1; j <= n; j++)
@@ -26,9 +28,11 @@ int main()
     {
         if(light[i])
         {
-            if(first) {first = 0;}
-            else {printf(" ");}
-            printf("%d", i);
+          //当不是第一个输出的时候，在输出数字之前还
+          //输出一个空格，即将输出的数字用空格隔开
+          if(first) {first = 0;}
+          else {printf(" ");}
+          printf("%d", i);
         }
     }
     printf("\n");
